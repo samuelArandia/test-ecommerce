043e89cbd6867a4321c7c7e77b4660ec832192fe
Samuel Arandia <sarandia@advise.cl>
2025-03-01 19:29:01 -0300

    First commit

---
+++ src/test/java/com/springboot/springmvc/app/testecommerce/TestEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springmvc.app.testecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
